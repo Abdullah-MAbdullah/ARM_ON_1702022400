f64dadc81958d70d09b91aed093fe5a6ad6f22a0
Abdullah MOUSTAFA <abdullah.moustafa@coretech-innovations.com>
2024-05-04 01:47:11 +0300

    Add the DMA Driver

---
+++ ARM_COTS/MCAL/DMA/DMA_config.h
@@ -0,0 +1,18 @@
+/****************************************************************************************
+* Author:				Abdullah M. Abdullah
+* Creation Data:		20 Apr, 2024
+* Version:				v1.0
+* Compiler:				GNU ARM-GCC
+* Controller:			STM32F401CCU6
+* Layer:				MCAL
+****************************************************************************************/
+/***************************************************************************************
+* Version	  Date				  	Author				  			Description
+* v1.0		  20 Apr, 2024			Abdullah M. Abdullah		  	Initial Creation
+***************************************************************************************/
+#ifndef DMA_CONFIG_H
+#define DMA_CONFIG_H
+
+
+
+#endif
